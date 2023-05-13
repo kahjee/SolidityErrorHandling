3b6cc35c3cc752f2e2b4b25fa962e46ae69d6a69
kahjee <89579589+kahjee@users.noreply.github.com>
2023-05-13 10:16:58 +0800

    Added boilerplate code to main.sol

--- main.sol
+++ main.sol
@@ -1,3 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+contract ErrorHandling {
+    uint public number;
+
+    function requirenumber(uint _testnumber) public {
+
+    }
+
+    function assertnumber(uint _testnumber) public {
+
+    }
+
+    function revertnumber(uint _testnumber) public {
+        
+    }
+
+}
