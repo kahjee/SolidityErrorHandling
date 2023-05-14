9efc54257c906f32040a71a27637d2b49e2fce28
kahjee <89579589+kahjee@users.noreply.github.com>
2023-05-14 12:39:27 +0800

    Added require, assert, and revert.

--- main.sol
+++ main.sol
@@ -5,15 +5,20 @@ contract ErrorHandling {
     uint public number;
 
     function requirenumber(uint _testnumber) public {
-
+        require(_testnumber > 0, "Number provided must be greater than 0");
+        number = _testnumber;
     }
 
     function assertnumber(uint _testnumber) public {
-
+        assert(_testnumber > 0);
+        number = _testnumber;
     }
 
     function revertnumber(uint _testnumber) public {
-        
+        if (number == 0){
+            revert("Number cannot be 0.");
+        }
+        number = _testnumber;
     }
 
 }
