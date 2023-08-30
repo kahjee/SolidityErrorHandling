f6f4f47e9fb6e50a89d6faf21a5005a04d290bf0
kahjee <89579589+kahjee@users.noreply.github.com>
2023-08-30 16:42:34 +0800

    Updated main.sol

--- main.sol
+++ main.sol
@@ -2,23 +2,25 @@
 pragma solidity 0.8.18;
 
 contract ErrorHandling {
-    uint public number;
+    uint public number1;
+    uint public number2;
+    uint public number3;
 
-    function requirenumber(uint _testnumber) public {
-        require(_testnumber > 0, "Number provided must be greater than 0");
-        number = _testnumber;
+    function requirenumber(uint _testnumber1) public {
+        require(_testnumber1 < 10, "Number provided must be less than 10");
+        number1 = _testnumber1;
+    } 
+
+    function assertnumber(uint _testnumber2) public {
+        assert(_testnumber2 > 0);
+        number2 = _testnumber2;
     }
 
-    function assertnumber(uint _testnumber) public {
-        assert(_testnumber > 0);
-        number = _testnumber;
-    }
-
-    function revertnumber(uint _testnumber) public {
-        if (number == 0){
+    function revertnumber(uint _testnumber3) public {
+        if (number3 == 0){
             revert("Number cannot be 0.");
         }
-        number = _testnumber;
+        number3 = _testnumber3;
     }
 
 }
